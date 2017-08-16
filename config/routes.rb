84a4cd565da2c9294c02b88713b88137880fcc72
hiracky16 <h.piiice16@gmail.com>
2017-08-16 16:33:44 +0900

    register user

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
   get 'static_pages/new'
 
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
