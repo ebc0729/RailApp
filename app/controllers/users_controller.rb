f7c52869c6cfead76d15ccb77d84ba5b46b1d2e3
hiracky16 <h.piiice16@gmail.com>
2017-08-16 17:21:04 +0900

    Login session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "ようこそ RailApp へ"
       redirect_to @user
     else
