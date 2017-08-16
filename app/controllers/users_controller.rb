73d621678737b28ab5598c84b9519fa467e7b7f3
hiracky16 <h.piiice16@gmail.com>
2017-08-16 14:44:03 +0900

    create login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,4 @@
 class UsersController < ApplicationController
-  def edit
-  end
-
-  def destroy
-  end
-
   def new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,6 @@ Rails.application.routes.draw do
 
   get 'rails/record'
 
-  get 'users/edit'
-
-  get 'users/destroy'
-
   get 'sessions/new'
 
   get 'sessions/destroy'
@@ -23,7 +19,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/new'
 
-  get 'users/new'
+  get  '/signup',  to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
