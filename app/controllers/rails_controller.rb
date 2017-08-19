7aefebac65c56dc66a4c70b0ed54874d44db2e80
hiracky16 <h.piiice16@gmail.com>
2017-08-19 16:07:49 +0900

    [WIP] function of recording

--- app/controllers/rails_controller.rb
+++ app/controllers/rails_controller.rb
@@ -7,5 +7,8 @@ class RailsController < ApplicationController
 
   def record
     @area = Area.all
+    @company = Company.where({area_id: 1})
+    @line = Line.where({company_id: 2})
+    @station = Station.where({rail_id: 3})
   end
 end
