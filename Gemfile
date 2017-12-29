9f9c0f805c287edb954a6db39ae8540960bac3d2
hiracky16 <h.piiice16@gmail.com>
2017-12-29 15:51:01 +0900

    Gem の追加と既存の User モデルとテーブルを削除

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'jquery-turbolinks'
 gem 'gon'
 gem 'dotenv-rails'
+
+gem 'devise'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816064724) do
+ActiveRecord::Schema.define(version: 20171229064859) do
 
   create_table "areas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "authentications", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id"
+    t.string "provider"
+    t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -37,15 +45,6 @@ ActiveRecord::Schema.define(version: 20170816064724) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
