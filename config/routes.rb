f6dc2ca11a388d89d32f5bcce61d3e8d3c96c8cf
hiracky16 <h.piiice16@gmail.com>
2017-12-29 22:17:36 +0900

    ルートの修正

--- config/routes.rb
+++ config/routes.rb
@@ -15,41 +15,17 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
 	
-  # get 'rails/show'
+  get 'rails/show'
 
-  # get 'rails/list'
+  get 'rails/list'
 
-  # get 'rails/record'
+  get 'rails/record'
 
   # get 'rails/confirmation'
 
   # get 'rails/registered'
 
-  # get 'sessions/new'
+  get 'static_pages/help'
 
-  # get 'sessions/destroy'
-
-  # get 'sessions/create'
-
-  # # root 'static_pages#index'
-
-  # get 'static_pages/help'
-
-  # get 'static_pages/destroy'
-
-  # get 'static_pages/new'
-
-  # get 'users/edit'
-  
-  # get 'users/show'
-
-  # get  '/signup',  to: 'users#new'
-  # post '/signup',  to: 'users#create'
-
-  # resources :users
-
-  # get    '/login',   to: 'sessions#new'
-  # post   '/login',   to: 'sessions#create'
-  # delete '/logout',  to: 'sessions#destroy'
-  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
