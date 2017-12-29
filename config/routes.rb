dbc29f68f32d36edc7542f4e8e54deeff542b6e8
hiracky16 <h.piiice16@gmail.com>
2017-12-29 19:28:45 +0900

    ユーザページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,53 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+  get 'home/show'
+
+  get 'index/show'
+
+  get 'index/edit'
+
+  root 'home#index'
+  get 'home/show'
+  get 'home/edit'
+
   devise_for :users
 	
-	root 'home#index'
+  # get 'rails/show'
 
-  get 'rails/show'
+  # get 'rails/list'
 
-  get 'rails/list'
+  # get 'rails/record'
 
-  get 'rails/record'
+  # get 'rails/confirmation'
 
-  get 'rails/confirmation'
+  # get 'rails/registered'
 
-  get 'rails/registered'
+  # get 'sessions/new'
 
-  get 'sessions/new'
+  # get 'sessions/destroy'
 
-  get 'sessions/destroy'
+  # get 'sessions/create'
 
-  get 'sessions/create'
+  # # root 'static_pages#index'
 
-  # root 'static_pages#index'
+  # get 'static_pages/help'
 
-  get 'static_pages/help'
+  # get 'static_pages/destroy'
 
-  get 'static_pages/destroy'
+  # get 'static_pages/new'
 
-  get 'static_pages/new'
-
-  get 'users/edit'
+  # get 'users/edit'
   
-  get 'users/show'
+  # get 'users/show'
 
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
+  # get  '/signup',  to: 'users#new'
+  # post '/signup',  to: 'users#create'
 
-  resources :users
+  # resources :users
 
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get    '/login',   to: 'sessions#new'
+  # post   '/login',   to: 'sessions#create'
+  # delete '/logout',  to: 'sessions#destroy'
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171229065255) do
+ActiveRecord::Schema.define(version: 20171229102719) do
 
   create_table "areas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20171229065255) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
