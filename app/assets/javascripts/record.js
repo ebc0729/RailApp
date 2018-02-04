b58816862ef97194eba6a64cd86eb30a11dacaad
hiracky16 <h.piiice16@gmail.com>
2018-02-04 16:04:50 +0900

    路線, 駅, 日付を設定する #33

--- app/assets/javascripts/record.js
+++ app/assets/javascripts/record.js
@@ -1,23 +1,70 @@
 $(document).ready(function(){
-    $('li.area').click(function(){
-        $('div#allcompany').hide();
-        $('div#allcompany').animate({'width': 'show'});
+    $('.area').click(function(){
+        $('#pulldown').hide();
+        $('#allroute').hide();
+        $('#allcompany').hide();
+        $('#allcompany').animate({'width': 'show'});
 
-        if($('ul#companies li').length > 0){
-            $('ul#companies').empty();
+        if($('#companies li').length > 0){
+            $('#companies').empty();
         }
 
         $(this).addClass('checked');
         $(this).siblings().removeClass('checked');
         var area_id = $(this).attr('id')
-        console.log(area_id);
         getCompanies(area_id)
     });
 
-    // $('li.company').click(function(){
-    //     $(this).addClass('checked');
-    //     $(this).siblings().removeClass('checked');
-    // });
+    $(document).on('click', '.company', function(){
+        $('#pulldown').hide();
+        $('#allroute').hide();
+        $('#allroute').animate({'width': 'show'});
+
+        if($('#routes li').length > 0){
+            $('#routes').empty();
+        }
+
+        $(this).addClass('checked');
+        $(this).siblings().removeClass('checked');
+        var company_id = $(this).attr('id');
+        getRoutes(company_id);
+    });
+
+    $(document).on('click', '.route', function(){
+        $('#pulldown').hide();
+        $('#pulldown').animate({'width': 'show'});
+
+        if($('#startStation option').length > 0){
+            $('#startStation').empty();
+        }
+
+        if($('#endStation option').length > 0){
+            $('#endStation').empty();
+        } 
+        
+        $(this).addClass('checked');
+        $(this).siblings().removeClass('checked');
+        var route_id = $(this).attr('id');
+        getStations(route_id);
+    });
+
+    $(document).on('click', '.route', function(){
+        $('#pulldown').hide();
+        $('#pulldown').animate({'width': 'show'});
+
+        if($('#startStation option').length > 0){
+            $('#startStation').empty();
+        }
+
+        if($('#endStation option').length > 0){
+            $('#endStation').empty();
+        } 
+        
+        $(this).addClass('checked');
+        $(this).siblings().removeClass('checked');
+        var route_id = $(this).attr('id');
+        getStations(route_id);
+    });
 });
 
 function getCompanies(area_id) {
@@ -31,5 +78,35 @@ function getCompanies(area_id) {
         $('ul#companies li').attr('class', 'company')
     }).fail(function(data){
         $('<p class="errorMessage">データがありません</p>').appendTo('ul#companies');
+    })
+}
+
+function getRoutes(company_id) {
+    $.ajax({
+        url: gon.api_base_url + '/companies/' + company_id + '/routes',
+        type: 'get'
+    }).done(function(data) {
+        data.map(function(r){
+            $('ul#routes').append('<li id=' + r.id + '>' + r.name + '</li>')
+        });
+        $('ul#routes li').attr('class', 'route');
+    }).fail(function(data){
+        $('<p class="errorMessage">データがありません</p>').appendTo('ul#routes');
+    })
+}
+
+function getStations(route_id) {
+    $.ajax({
+        url: gon.api_base_url + '/routes/' + route_id + '/stations',
+        type: 'get'
+    }).done(function(data) {
+        data.map(function(s){
+            $('#startStation').append('<option value=' + s.id + '>' + s.name + '</option>');
+            $('#endStation').append('<option value=' + s.id + '>' + s.name + '</option>');
+        });
+        $('#startStation option').attr('class', 'start');
+        $('#endStation option').attr('class', 'end');
+    }).fail(function(data){
+        $('<p class="errorMessage">データがありません</p>').appendTo('pulldown');
     })
 }
