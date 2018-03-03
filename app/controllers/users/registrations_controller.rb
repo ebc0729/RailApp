043d0758b9047ebc749580a99c8d4c55d8b9236d
hiracky16 <h.piiice16@gmail.com>
2018-03-03 14:50:41 +0900

    #35 年代と性別をセレクトボックス化

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,7 +42,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :age, :gender, :favorite_route])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :age, :gender, :favorite_route])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
