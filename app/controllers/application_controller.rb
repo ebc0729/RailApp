df4bc76586849745dd2dc836bf16ca322c8f47aa
hiracky16 <h.piiice16@gmail.com>
2018-03-03 13:50:59 +0900

    #35 登録フォーム、ユーザ情報変更画面に適応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,4 @@ class ApplicationController < ActionController::Base
     @current_user ||= current_user
   end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-  end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   def new
@@ -24,12 +24,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # DELETE /resource
   def destroy
-    if current_user == resource && current_user.admin?
-      flash[:error] == "You can't delete yourself"
-      redirect_to user_path(resource)
-    else
-      super
-    end
+    super
   end
 
   # GET /resource/cancel
@@ -42,15 +37,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   protected
-
+  
+  
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :age, :gender, :favorite_route])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:favorite_route])
   end
 
   # The path used after sign up.
